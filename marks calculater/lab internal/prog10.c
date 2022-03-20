26ead425fb175d390270f234353151e245624112
Kushvith <67113133+Kushvith@users.noreply.github.com>
2022-03-20 21:39:37 +0530

    reformatting update

--- marks calculater/lab internal/prog10.c
+++ marks calculater/lab internal/prog10.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 
 struct Node
 {
@@ -11,138 +11,155 @@ struct Node
 };
 typedef struct Node nodes;
 
-nodes *create(nodes *node,int elem){
+nodes *create(nodes *node, int elem)
+{
     nodes *newnode;
-    if(node == NULL){
+    if (node == NULL)
+    {
         newnode = (nodes *)malloc(sizeof(nodes));
         newnode->data = elem;
         newnode->left = newnode->right = NULL;
         return newnode;
     }
-    else if(elem<node->data){
-        node->left = create(node->left,elem);
+    else if (elem < node->data)
+    {
+        node->left = create(node->left, elem);
     }
-    else if(elem>node->data)
-        node->right = create(node->right,elem);
-    return node;    
+    else if (elem > node->data)
+        node->right = create(node->right, elem);
+    return node;
 }
-nodes *findmin(nodes *node){
-    if(node == NULL)
+nodes *findmin(nodes *node)
+{
+    if (node == NULL)
         return NULL;
     else if (node->left)
     {
         /* code */
         node = findmin(node->left);
     }
-    else if(node->right)
+    else if (node->right)
         node = findmin(node->right);
-    return node;    
-        
+    return node;
 }
-nodes *search(nodes *node,int key){
-    if(node== NULL)
-    {    
+nodes *search(nodes *node, int key)
+{
+    if (node == NULL)
+    {
         printf("element not found");
         return node;
     }
-    else if(key>node->data)
-    search(node->right,key);
-    else if(key <node->data)    
-     search(node->left,key);
-    else{
-        printf("element found %d\n",node->data);
-    } 
+    else if (key > node->data)
+        search(node->right, key);
+    else if (key < node->data)
+        search(node->left, key);
+    else
+    {
+        printf("element found %d\n", node->data);
+    }
     return node;
 }
-void inorder(nodes *node){
-    if(node!=NULL){
+void inorder(nodes *node)
+{
+    if (node != NULL)
+    {
         inorder(node->left);
-        printf("%d",node->data);
+        printf("%d", node->data);
         inorder(node->right);
     }
 }
-void preorder(nodes *node){
-    if(node!=NULL){
-        printf("%d",node->data);
+void preorder(nodes *node)
+{
+    if (node != NULL)
+    {
+        printf("%d", node->data);
         preorder(node->left);
         preorder(node->right);
     }
 }
-void postorder(nodes *node){
-    if(node!=NULL){
+void postorder(nodes *node)
+{
+    if (node != NULL)
+    {
         postorder(node->left);
         postorder(node->right);
-        printf("%d",node->data);
+        printf("%d", node->data);
     }
 }
-nodes *del(nodes *node,int elem){
+nodes *del(nodes *node, int elem)
+{
     nodes *temp;
-    if(node == NULL)
+    if (node == NULL)
         printf("element not found\n");
-    else if(elem<node->data)
-        node->left = del(node->left,elem);
-    else if(elem>node->data)
-        node->right = del(node->right,elem);
-    else {
-        if(node->right&&node->left){
+    else if (elem < node->data)
+        node->left = del(node->left, elem);
+    else if (elem > node->data)
+        node->right = del(node->right, elem);
+    else
+    {
+        if (node->right && node->left)
+        {
             temp = findmin(node->right);
             node->data = temp->data;
-            node->right= del(node,elem);
+            node->right = del(node->right, temp->data);
             free(temp);
-        }    
-        else{
-            temp = node;
-            if(node->left == NULL)
-                node = node->right;
-            else if(node->right == NULL)
-                node = node->left;    
         }
-    }    
+        else
+        {
+            temp = node;
+            if (node->left == NULL)
+                node = node->right;
+            else if (node->right == NULL)
+                node = node->left;
+        }
+    }
     return node;
 }
-void main(){
-    int ch,i,n,elem;
+void main()
+{
+    int ch, i, n, elem;
     nodes *node;
     node = NULL;
-    while(ch!=7){
+    while (ch != 7)
+    {
         printf("1.create\t2.search\t3.inorder\t4.postorder\t5.preorder\t6.del\t7.exit\n");
-        scanf("%d",&ch);
+        scanf("%d", &ch);
         switch (ch)
         {
         case 1:
             printf("enter the number of elements to be created\n");
-            scanf("%d",&n);
-            for(i=0;i<n;i++)
+            scanf("%d", &n);
+            for (i = 0; i < n; i++)
             {
-                printf("enter the element %d",i+1);
-                scanf("%d",&elem);
-                node = create(node,elem);
+                printf("enter the element %d", i + 1);
+                scanf("%d", &elem);
+                node = create(node, elem);
             }
             break;
         case 2:
-           printf("enter the search key\n");
-           scanf("%d",&elem);
-           search(node,elem);
+            printf("enter the search key\n");
+            scanf("%d", &elem);
+            search(node, elem);
             break;
         case 3:
-           inorder(node);
-            break;     
+            inorder(node);
+            break;
         case 4:
-           postorder(node);
-            break; 
+            postorder(node);
+            break;
         case 5:
-           preorder(node);
-            break;    
+            preorder(node);
+            break;
         case 6:
-         printf("enter the element to be deleted\n");
-         scanf("%d",&elem);
-           node = del(node,elem);
-            break;    
-            case 7:
+            printf("enter the element to be deleted\n");
+            scanf("%d", &elem);
+            node = del(node, elem);
+            break;
+        case 7:
             exit(0);
-            break;      
+            break;
         default:
-        printf("invalid choice\n");
+            printf("invalid choice\n");
             break;
         }
     }
--- marks calculater/lab internal/prog9.c
+++ marks calculater/lab internal/prog9.c
@@ -1,138 +1,156 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <math.h>
 struct Node
 {
     /* data */
-    int flag,px,py,pz,cf;
+    int flag, px, py, pz, cf;
     struct Node *next;
 };
 
 typedef struct Node node;
 
-node *getnode(){
+node *getnode()
+{
     node *newnode;
-    newnode = (node*)malloc(sizeof(node));
-    if(newnode == NULL){
+    newnode = (node *)malloc(sizeof(node));
+    if (newnode == NULL)
+    {
         printf("insufficent memory\n");
         return 0;
     }
     else
-        return newnode;    
+        return newnode;
 }
-node *insert_rear(int cf,int x,int y,int z,node *head){
-    node *temp,*cur;
+node *insert_rear(int cf, int x, int y, int z, node *head)
+{
+    node *temp, *cur;
     temp = getnode();
     temp->cf = cf;
     temp->px = x;
     temp->py = y;
     temp->pz = z;
     cur = head;
-    while(cur->next!=head)
+    while (cur->next != head)
         cur = cur->next;
     cur->next = temp;
     temp->next = head;
-    return head;    
+    return head;
 }
-node *read_poly(node *head){
-    int px,py,pz,ch=1,cf;
-    while(ch!=0){
+node *read_poly(node *head)
+{
+    int px, py, pz, ch = 1, cf;
+    while (ch != 0)
+    {
         printf("enter the co-efficent\n");
-        scanf("%d",&cf);
+        scanf("%d", &cf);
         printf("enter the power value of x,y,z if(0 indicates no term)\n");
-        scanf("%d%d%d",&px,&py,&pz);
+        scanf("%d%d%d", &px, &py, &pz);
         printf("3");
-        head = insert_rear(cf,px,py,pz,head);
+        head = insert_rear(cf, px, py, pz, head);
         printf("2");
         printf("do you want to enter one more term (if no enter 0)\n");
-        scanf("%d",&ch);
+        scanf("%d", &ch);
         return head;
     }
 }
-node *add_poly(node *h1,node *h2,node *h3){
-    node *p1,*p2;
-    int x1,y1,z1,cf1,x2,y2,z2,cf,cf2;
+node *add_poly(node *h1, node *h2, node *h3)
+{
+    node *p1, *p2;
+    int x1, y1, z1, cf1, x2, y2, z2, cf, cf2;
     p1 = h1->next;
-    while(p1!=h1){
+    while (p1 != h1)
+    {
         x1 = p1->px;
         y1 = p1->py;
         z1 = p1->pz;
         cf1 = p1->cf;
         p2 = h2->next;
-        while(p2!=h2){
+        while (p2 != h2)
+        {
             x2 = p2->px;
             y2 = p2->py;
             z2 = p2->pz;
             cf2 = p2->cf;
-            if(x1==x2&&y1==y2&&z1==z2)
+            if (x1 == x2 && y1 == y2 && z1 == z2)
                 break;
-            p2 = p2->next;    
+            p2 = p2->next;
         }
-        if(p2!=h2){
-            cf = cf1+cf2;
+        if (p2 != h2)
+        {
+            cf = cf1 + cf2;
             p2->flag = 1;
-            if(cf!=0)
-               h3 =  insert_rear(cf,x1,y1,z1,h3);
+            if (cf != 0)
+                h3 = insert_rear(cf, x1, y1, z1, h3);
         }
-        else{
-            h3 = insert_rear(cf1,x1,y1,z1,h3);
+        else
+        {
+            h3 = insert_rear(cf1, x1, y1, z1, h3);
         }
         p1 = p1->next;
     }
     p2 = p2->next;
-    while (p2!=h2)
+    while (p2 != h2)
     {
         /* code */
-        if(p2->flag!=1){
-          h3 =   insert_rear(p2->cf,p2->px,p2->py,p2->pz,h3);
+        if (p2->flag != 1)
+        {
+            h3 = insert_rear(p2->cf, p2->px, p2->py, p2->pz, h3);
         }
         p2 = p2->next;
     }
     return h3;
 }
-void evaluate(node *head){
-    int x,y,z;
+void evaluate(node *head)
+{
+    int x, y, z;
     float result = 0;
     node *p;
     p = head->next;
     printf("enter the value of x,y,z\n");
-    scanf("%d%d%d",&x,&y,&z);
-    while (p!=head)
+    scanf("%d%d%d", &x, &y, &z);
+    while (p != head)
     {
         /* code */
-        printf("%d",p->cf);
-        result = result+((p->cf)*(pow(x,p->px))*(pow(y,p->py))*(pow(z,p->pz)));
+        printf("%d", p->cf);
+        result = result + ((p->cf) * (pow(x, p->px)) * (pow(y, p->py)) * (pow(z, p->pz)));
         p = p->next;
     }
-    printf("the result is %f\n",result);
+    printf("the result is %f\n", result);
 }
-void display(node *head){
+void display(node *head)
+{
     node *p;
-    if(head->next == head)
+    if (head->next == head)
         printf("polynomial does not exists\n");
-    else{
+    else
+    {
         p = head->next;
-        while(p!=head){
-            printf("%dx^%dy^%dz^%d\n",p->cf,p->px,p->py,p->pz);
-            if(p->next!=head)
-             printf("+");
-            p = p->next; 
+        while (p != head)
+        {
+            printf("%dx^%dy^%dz^%d\n", p->cf, p->px, p->py, p->pz);
+            if (p->next != head)
+                printf("+");
+            p = p->next;
         }
-    }    
+    }
 }
-void main(){
-    node *h1,*h2,*h3;int choice;
+void main()
+{
+    node *h1, *h2, *h3;
+    int choice;
     h1 = getnode();
     h2 = getnode();
     h3 = getnode();
     h1->next = h1;
     h2->next = h2;
     h3->next = h3;
-    while(choice!=3){
+    while (choice != 3)
+    {
         printf("1.evaluating polynomial\n2.adding two polynomial\n 3.exit\n");
         printf("enter the choice\n");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
         switch (choice)
         {
         case 1:
@@ -149,14 +167,15 @@ void main(){
             h2 = read_poly(h2);
             printf("the polynomial 2:\n");
             display(h2);
-            h3 = add_poly(h1,h2,h3);
+            h3 = add_poly(h1, h2, h3);
             printf("the polynomial addition is :\n");
             display(h3);
             break;
-        case 3: exit(0);       
-        
+        case 3:
+            exit(0);
+
         default:
-        printf("invalid choice\n");
+            printf("invalid choice\n");
             break;
         }
     }
